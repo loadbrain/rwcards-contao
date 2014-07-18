b0f2b867dd5fab7a728d92eec8288939f88ee721
Ralf <ralf.weber@ub.uni-freiburg.de>
2014-07-18 11:05:39 +0200

    Import from GoolgeCode

---
+++ SendCardRwCards.php
@@ -0,0 +1,248 @@
+<?php
+
+if (!defined('TL_ROOT'))
+die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2011 Ralf Weber
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  LoadBrain 2011
+ * @author     Ralf Weber <http://www.loadbrain.de>
+ * @package    Comments
+ * @license    LGPL
+ * @filesource
+ */
+
+/**
+ * Class SendCardRwCards
+ *
+ * @copyright  LoadBrain 2011
+ * @author     Ralf Weber <http://www.loadbrain.de>
+ * @package    Controller
+ */
+class SendCardRwCards extends Frontend {
+
+	/**
+	 * Add all cards of one category to a template
+	 * @param object
+	 * @param object
+	 * @param string
+	 * @param integer
+	 */
+	public function addToTemplate($objTemplate, $objConfig, $strSource, $intParent, $alias) {
+	global $objPage;
+		$this->import("Session");
+		$this->sessionData = $this->Session->getData();
+
+
+
+		$this->firstReceiver = true;
+		$this->lastId = array();
+		$this->linkToRWCards = array();
+		$this->is_enduser = (\Input::get('sessionId') != "") ? 1 : 0;
+		$this->sessionId = (\Input::get('sessionId') != "" ) ? \Input::get('sessionId') : false;
+		$this->category_id = (\Input::get('category_id') > 0 ) ? \Input::get('category_id') : false;
+		$this->card_id = (\Input::get('id') > 0 ) ? \Input::get('id') : false;
+		$this->viewCardOnly = (\Input::get('view') == 'rwcardsReWriteCard' ) ? true : false;
+		$this->additionalReceivers = $this->Session->get('additionalReceivers');
+
+
+
+		if ($objConfig->template == '') {
+			$objTemplate->template = 'rwcards_sendcard';
+		}
+
+
+		/**
+		 * set some vars
+		 */
+		$GLOBALS['TL_CSS'][''] = \Environment::get('base') . "system/modules/rwcards/assets/css/rwcards.previewandsend.css";
+		$objTemplate->data = $this->sessionData;
+		$objTemplate->rwcards_sendcard_invalid_code = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_invalid_code'];
+		$objTemplate->rwcards_sendcard_sucessfully_sent = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_sucessfully_sent'];
+		$objTemplate->rwcards_sendcard_view_your_card = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_view_your_card'];
+		$objTemplate->rwcards_sendcard_congratulations = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_congratulations'];
+		$objTemplate->rwcards_sendcard_view_your_card_hint = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_view_your_card_hint'];
+		$objTemplate->rwcards_sendcard_show_front_card = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_show_front_card'];
+		$objTemplate->rwcards_sendcard_show_back_card = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_show_back_card'];
+		$objTemplate->rwcards_sendcard_from = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_from'];
+		$objTemplate->rwcards_sendcard_name_from = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_name_from'];
+		$objTemplate->rwcards_sendcard_message = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_message'];
+		$objTemplate->rwcards_sendcard_to = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_to'];
+		$objTemplate->rwcards_sendcard_name_to = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_name_to'];
+		$objTemplate->rwcards_sendcard_reply = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_reply'];
+		$objTemplate->is_enduser = $this->is_enduser;
+		$objTemplate->reWritetoSender = (\Input::get('view') == "rwcardsReWriteCard") ? 1 : 0;
+		$objTemplate->sessionId = (\Input::get('view') == "rwcardsReWriteCard") ? \Input::get('sessionId') : null;
+		$objTemplate->alias = $alias;
+
+
+		if (\Input::get('view') == "rwcardsReWriteCard") {
+			// New Card or someone answers
+			if ( $this->sessionId != ""){ 
+				$resCats = $this->Database->prepare("select * from tl_rwcardsdata where id = '" . $this->card_id . "' and tl_rwcardsdata.sessionId = '" . $this->sessionId ."'");
+				$this->data = $resCats->execute()->fetchAllAssoc();
+				$reWritetoSenderId = $this->Database->prepare("select id from tl_rwcards where picture = '" . $this->data[0]['picture'] . "'")->execute()->row();
+				$objTemplate->reWritetoSenderId = $reWritetoSenderId['id'];
+				$objTemplate->data = $this->data;
+				$this->setCardStatusToRead();
+			}
+		} else {
+			//Send Card -> view: rwcardssendcard
+
+			if (count($this->Session->getData()) > 0) {		
+				$objRes = $this->Database->prepare("INSERT INTO tl_rwcardsdata SET picture= '" . $this->Session->get('picture')
+				. "', nameTo = '" . $this->Session->get('rwcardsReceiver')
+				. "', nameFrom = '" . $this->Session->get('rwNameFrom')
+				. "', emailTo = '" . $this->Session->get('rwCardsFormEmailTo')
+				. "', emailFrom = '" . $this->Session->get('rwEmailFrom')
+				. "', message = '" . addslashes($this->Session->get('rwMessage'))
+				. "', sessionId = '" . $_SESSION['rwcards']['rwcards_session']
+				. "' , writtenOn = '" . date("Y-m-d")
+				. "' , cardSent = '0'")->execute();
+				// Get the ID generated from the previous INSERT operation
+	
+				//$this->lastId[0] = $objRes->insertId;
+				array_push($this->lastId, $objRes->insertId);
+
+				// All receivers are stored in tbe database
+				for ($i = 1; $i <= $this->additionalReceivers; $i++) {
+					$this->Database->prepare("INSERT INTO tl_rwcardsdata SET picture= '" . $this->Session->get('picture')
+					. "', nameTo = '" . $this->Session->get('rwcardsReceiver' . $i)
+					. "', nameFrom = '" . $this->Session->get('rwNameFrom')
+					. "', emailTo = '" . $this->Session->get('rwCardsFormEmailTo' . $i)
+					. "', emailFrom = '" . $this->Session->get('rwEmailFrom')
+					. "', message = '" . addslashes($this->Session->get('rwMessage'))
+					. "', sessionId = '" . $_SESSION['rwcards']['rwcards_session']
+					. "' , writtenOn = '" . date("Y-m-d")
+					. "' , cardSent = '0'")->execute();
+
+
+
+					// Get the ID generated from the previous INSERT operation
+					$this->lastId[$i] = $objRes->insertId;
+
+					//$this->sendEcardPerMail($objTemplate);
+				}
+				$this->sendEcardPerMail($objTemplate);
+			}
+			return;
+		}
+	}
+
+	/**
+	 * Send an email to the receiver(s)
+	 */
+	public function sendEcardPerMail($objTemplate) {
+	global $objPage;
+		$this->import("String");
+		$objEmail = new \Email();
+
+		for ($i = 0; $i < count($this->lastId); $i++) {
+
+			$this->linkToRWCards[$i] = \Environment::get('base') . \Controller::generateFrontendUrl($objPage->row()). '?view=rwcardsReWriteCard&sessionId=' . $_SESSION['rwcards']['rwcards_session'] . '&id=' . $this->lastId[$i] . '&read=1&sendmail=1';
+			$this->linkToViewOnly[$i] = \Environment::get('base') . \Controller::generateFrontendUrl($objPage->row()) . '?view=rwcardsReWriteCard&sessionId=' . $_SESSION['rwcards']['rwcards_session'] . '&id=' . $this->lastId[$i] . '&sendmail=0';
+
+			// send card as attachement
+			if ($_SESSION['rwcards']['config']['rwcards_per_attachement']) {
+				$message[$i] = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_greeting'] . " "
+				. $this->Session->get('rwcardsReceiver' . $i) . "\n\n"
+				. $this->Session->get('rwNameFrom') . " " . $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_1'] . "\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_2'] . "\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_3'] . "\n\n"
+				. nl2br($this->Session->get('rwMessage'))
+				. "\n\n" . $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_separator'] . "\n\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_copyright'];
+				$objEmail->attachFile($this->Session->get('picture'));
+			} else {
+
+				$rwcardsReceiver = ($i > 0 ) ? trim($this->Session->get('rwcardsReceiver' . $i)) . "\n\n" : trim($this->Session->get('rwcardsReceiver')) . "\n\n";
+
+				$message[$i] = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_greeting'] . " "
+				. $rwcardsReceiver
+				. $this->Session->get('rwNameFrom')
+				. " " . $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_part_1'] . "\n\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_part_2'] . "\n\n"
+				. $this->linkToRWCards[$i] . "\n\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_separator'] . "\n"
+				. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_copyright'] . "\n\n";
+			}
+
+			$objEmail->from = $this->Session->get('rwEmailFrom');
+			$objEmail->fromName = $this->Session->get('rwNameFrom');
+			$objEmail->subject = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_subject'] . $this->Session->get('rwNameFrom');
+
+			$message[$i] = strip_tags($message[$i]);
+			$message[$i] = $this->String->decodeEntities($message[$i]);
+			$message[$i] = str_replace(array('[&]', '[lt]', '[gt]'), array('&', '<', '>'), $message[$i]);
+			$objEmail->text = sprintf($message[$i]);
+
+			// Send for additional reveivers
+			for ($a = 1; $a <= $this->additionalReceivers; $a++) {
+				$objEmail->sendTo($this->Session->get('rwCardsFormEmailTo' . $a)); //'ralf@localhost'
+				$res = $this->Database->prepare("update tl_rwcardsdata set cardSent = '1' WHERE id = '" . $this->lastId[$a] . "'")->execute();
+				if ($this->additionalReceivers == $a) {
+					$this->additionalReceivers = null;
+				}
+			}
+		}
+        // Send to first receiver
+		$objEmail->sendTo($this->Session->get('rwCardsFormEmailTo')); //'ralf@localhost';
+        $res = $this->Database->prepare("update tl_rwcardsdata set cardSent = '1' WHERE id = '" . $this->lastId[0] . "'")->execute();
+        session_destroy();
+        return;
+	}
+
+	/**
+	 * Set the status of the card to read and send an email to the receiver that the card was read
+	 */
+	public function setCardStatusToRead() {
+		if ( $this->data[0]['cardRead'] == '0'){
+			$res = $this->Database->prepare("update tl_rwcardsdata set cardRead = '1', readOn = '" . date("Y-m-d") . "' WHERE id = '" . \Input::get('id') . "' and sessionId = '" . $this->sessionId . "'")->execute();
+			//$this->data = $res->execute()->fetchAllAssoc();
+
+			$objEmail = new Email();
+			$objEmail->subject =  $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_subject'];
+			$message = $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_greeting'] . " "
+			. $this->data[0]['nameTo'] . ",\n\n"
+			. $this->data[0]['nameFrom'] . " <" . $this->data[0]['emailFrom'] . "> "
+			. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_1'] . " "
+			. date("d.m.Y") . " "
+			. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_2'] . "\n\n"
+			. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_3'] . "\n\n"
+			. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_separator'] . "\n"
+
+			. $GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_copyright'] . "\n\n";
+
+			$objEmail->text = sprintf($message);
+			$objEmail->from = $this->data[0]['emailTo'] ;
+			$objEmail->fromName = $this->data[0]['nameTo'];
+			$objEmail->sendTo($this->data[0]['emailFrom']);
+			// update table so email is only sent once
+			$query = "update #__rwcardsdata set cardRead = '1', readOn = '" . date("Y-m-d")
+			. "' where sessionId = '" . $sessionId
+			. "' and id = '" . $id . "'";
+		}
+	}
+
+}
+
+?>
---
+++ languages/de/default.php
@@ -0,0 +1,101 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Ralf Weber
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  LoadBrain 2011
+ * @author     Ralf Weber <http://www.loadbrain.de>
+ * @package    Calendar
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * ListCards
+ */
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_listcards_see_all_cards'] = 'Zeige alle Karten dieser Kategorie';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_listcards_no_category_published_or_created'] = "Noch keine Kategorien erzeugt oder veröffentlicht!";
+
+/**
+ * One category -> ListCards
+ */
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_one_category_listcards'] = 'Auf eine Karte zur Vollansicht klicken';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_one_category_chosen_category'] = "Gewählte Kategorie";
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_one_category_send_this_image'] = "Diese Karte verschicken";
+
+/**
+ * Fill out card
+ */
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_please_fill_out'] = 'Bitte alle Felder ausfüllen';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_name_from'] = 'Ihr Name';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_email_from'] = 'Ihre E-Mail Adresse';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_message'] = 'Ihre Nachricht';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_name_to'] = 'Name des Empfängers';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_email_to'] = 'E-Mail Adresse des Empfängers';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_add_receiver'] = 'Empfänger hinzufügen';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_remove_receiver'] = 'Empfänger entfernen';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_back'] = 'Zurück';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_preview_card'] = 'Vorschau';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_fillout_cards_send_card'] = 'Abschicken';
+
+/**
+ * Preview Cards
+ */
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_card'] = 'Voransicht der Karte';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_show_front_card'] = 'Vorderseite';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_show_back_card'] = 'Rückseite';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_from'] = 'Absender:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_message'] = 'Nachricht:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_preview_to'] = 'Empfänger:';
+
+/**
+ * Send Cards
+ */
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_invalid_code'] = 'Falscher Code. Bitte E-Mail nach korrekter URL durchsehen.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_sucessfully_sent'] = 'Ihre E-Card wurde erfolgreich verschickt';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_view_your_card'] = 'Ihre E-Card von';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_congratulations'] = 'Herzlichen Glückwunsch!';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_view_your_card_hint'] = 'Klicken Sie auf "Zeige hintere Ansicht" um zu antworten!';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_congatulations_hint'] = 'Sie werden eine Bestätigungs E-Mail bekommen, sobald der Empfänger die Karte gelesen hat.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_show_front_card'] = 'Zeige vordere Ansicht';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_show_back_card'] = 'Zeige hintere Ansicht';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_from'] = 'Von:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_name_from'] = 'Name';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_message'] = 'Nachricht';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_to'] = 'An:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_name_to'] = 'Name des Empfämgers';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_reply'] = 'jetzt antworten';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_subject'] = 'Sie haben eine ECard erhalten! Absender: ';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_greeting'] = 'Hallo';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_1'] = 'hat Ihnen eine ECard geschickt.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_2'] = 'Im Attachement das Bild der ECard.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_attachement_3'] = 'Hier seine Nachricht:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_separator'] = '----------------------------------------------------------------------';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_copyright'] = 'RWCards - Copyright Ralf Weber / LoadBrain - http://www.loadbrain.de/';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_part_1'] = 'hat Ihnen eine ECard geschickt.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_msg_part_2'] = 'Klicken Sie auf den folgenden Link um Ihre ECard anzusehen:';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_subject'] = 'Ihre ECard wurde gelesen!';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_1'] = 'hat Ihre Karte am';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_2'] = 'gelesen.';
+$GLOBALS['TL_LANG']['tl_rwcards']['rwcards_sendcard_read_msg_3'] = 'Weiterhin viel Spass mit RWCards!';
+
+?>
